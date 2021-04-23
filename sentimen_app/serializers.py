00f38f1e8238a9a1f26fe5f813fe0b93b2049c11
rshnewarray <rsh.newarray@gmail.com>
2021-04-23 14:01:46 +0800

    fix url

--- sentimen_app/serializers.py
+++ sentimen_app/serializers.py
@@ -6,23 +6,24 @@ import requests
 from bs4 import BeautifulSoup
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
-    url = "https://reviews.femaledaily.com/products/blush-76/blush/wardah/blush-on-71?cat=&cat_id=0&age_range=&skin_type=&skin_tone=&skin_undertone=&hair_texture=&hair_type=&order=newest&page="
     review = serializers.SerializerMethodField('getReview')
 
     def getReview(self, instance):
+        url = instance.url
+        url = url[:-1]
         request_object = self.context['request']
         jml = request_object.query_params.get('jumlah')
         
         data = []
 
         for i in range(int(jml)):
-            page = requests.get(self.url+""+str(i+1))
+            page = requests.get(url+""+str(i+1))
             review = serializers.SerializerMethodField('getReview')
 
             soup = BeautifulSoup(page.content, 'html.parser')
             for el in soup.find_all("p", {"class": "text-content"}):
                 data.append(el.text)
-
+ 
         return data
 
 
